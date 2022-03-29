a8c3736ea46b15e23ddbae1313069ec43cf323a1
Karthikeyan S <karthikkevin83@gmail.com>
2022-03-29 07:57:18 +0530

    commited

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Id = 1,
+                Name = "Testing Name"
+            };
+            // View or Partial View
+            //return View(movie);
+
+            // Content
+            //return Content("Hello  Guys");
+
+            // HttpNotFound
+            //return HttpNotFound();
+
+            // Empty Result
+            //return new EmptyResult();
+
+            // Redirect to Action Result
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
